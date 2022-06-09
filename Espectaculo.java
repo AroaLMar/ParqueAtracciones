6720964b1a442285d2985aa5794c91e56f1b4752
AroaLMar <104788399+AroaLMar@users.noreply.github.com>
2022-06-10 01:03:23 +0200

    Update Espectaculo.java

--- Espectaculo.java
+++ Espectaculo.java
@@ -1,45 +1,56 @@
 package com.company;
 
+
 import java.util.ArrayList;
 
-public class Espectaculo {
+    public class Espectaculo {
 
-    private String nombre;
-    private String tipoEspectaculo;
-    private Integer minutos;
-    private ArrayList <Pase> pases;
+        private String nombre;
+        private String tipoEspectaculo;
+        private int minutos;
+        private ArrayList <Pase> pases;
 
-    public Espectaculo(String nombre, String tipoEspectaculo, Integer minutos, ArrayList <Pase> pases) {
-        this.nombre = nombre;
-        this.tipoEspectaculo = tipoEspectaculo;
-        this.minutos = minutos;
-        this.pases=pases;
+        public void validarEspectaculo(String nombre, String tipoEspectaculo, int minutos) throws ParametrosNoValidos {
+            this.nombre = nombre;
+            if(nombre==null) throw new ParametrosNoValidos("El nombre del espectaculo no puede ser nulo");
+            this.tipoEspectaculo = tipoEspectaculo;
+            if(tipoEspectaculo==null) throw new ParametrosNoValidos("El tipo de espectaculo no puede ser nulo");
+            this.minutos = minutos;
+            if(minutos<=0)throw new ParametrosNoValidos("Los minutos no pueden ser menor o igual a 0");
+
+        }
+
+        public Espectaculo(String nombre, String tipoEspectaculo, int minutos, ArrayList <Pase> pases) throws ParametrosNoValidos {
+            validarEspectaculo(nombre, tipoEspectaculo, minutos);
+            this.pases=pases;
+            if(pases==null)throw new ParametrosNoValidos("El pase no puede ser nulo");
+
+        }
+
+        public Espectaculo(String nombre, String tipoEspectaculo, int minutos) throws ParametrosNoValidos {
+            validarEspectaculo(nombre, tipoEspectaculo, minutos);
+            this.pases=new ArrayList<Pase>();
+
+
+        }
+
+        @Override
+        public String toString() {
+            return "Espectaculo{" +
+                    "nombre='" + nombre + '\'' +
+                    ", tipoEspectaculo='" + tipoEspectaculo + '\'' +
+                    ", minutos=" + minutos +
+                    pases+
+                    '}';
+        }
+
+        public void añadirPases(Pase pases){
+
+            this.pases.add(pases);
+        }
+
+
+
+
     }
 
-    public Espectaculo(String nombre, String tipoEspectaculo, Integer minutos) {
-        this.nombre = nombre;
-        this.tipoEspectaculo = tipoEspectaculo;
-        this.minutos = minutos;
-        this.pases=new ArrayList<Pase>();
-
-    }
-
-    @Override
-    public String toString() {
-        return "Espectaculo{" +
-                "nombre='" + nombre + '\'' +
-                ", tipoEspectaculo='" + tipoEspectaculo + '\'' +
-                ", minutos=" + minutos +
-                pases+
-                '}';
-    }
-
-    public void añadirPases(Pase pases){
-
-        this.pases.add(pases);
-    }
-
-
-
-
-}
