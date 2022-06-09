9b4657f330ebd67047f280be6a90e8ff30993725
AroaLMar <104788399+AroaLMar@users.noreply.github.com>
2022-06-10 01:01:44 +0200

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,31 +1,31 @@
 package com.company;
 
+    public class Cliente {
 
-public class Cliente {
-
-    private String nombre;
-    private String apellidos;
-
+        private String nombre;
+        private String apellidos;
 
 
-    public Cliente(String nombre, String apellidos) throws Exception {
-        this.nombre = nombre;
-        if(nombre==null)throw new Exception("El nombre del cliente no puede ser nulo");
-        this.apellidos = apellidos;
-        if(apellidos==null)throw new Exception("Es obligatorio que los clientes tengan apellidos registrados");
+
+        public Cliente(String nombre, String apellidos) throws Exception {
+            this.nombre = nombre;
+            if(nombre==null)throw new Exception("El nombre del cliente no puede ser nulo");
+            this.apellidos = apellidos;
+            if(apellidos==null)throw new Exception("Es obligatorio que los clientes tengan apellidos registrados");
+
+        }
+
+
+
+        @Override
+        public String toString() {
+            return "Cliente{" +
+                    "nombre='" + nombre + '\'' +
+                    ", apellidos='" + apellidos + '\'' +
+                    '}';
+        }
+
+
 
     }
 
-
-
-    @Override
-    public String toString() {
-        return "Cliente{" +
-                "nombre='" + nombre + '\'' +
-                ", apellidos='" + apellidos + '\'' +
-                '}';
-    }
-
-
-
-}
