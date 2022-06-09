520d9eb35b5741cba12c032efb4dd20cbe8044ec
AroaLMar <104788399+AroaLMar@users.noreply.github.com>
2022-06-10 01:06:22 +0200

    Update Restaurante.java

--- Restaurante.java
+++ Restaurante.java
@@ -1,29 +1,37 @@
 package com.company;
 
-import java.sql.Time;
+
+import java.time.LocalDateTime;
 
 public class Restaurante {
 
-    private String nombre;
-    private int horarioInicio;
-    private int horarioFin;
-    private double precioMedioPersona;
+        private String nombre;
+        private LocalDateTime horarioInicio;
+        private LocalDateTime horarioFin;
+        private double precioMedioPersona;
 
 
-    public Restaurante(String nombre, int horarioInicio, int horarioFin, double precioMedioPersona) {
-        this.nombre = nombre;
-        this.horarioInicio = horarioInicio;
-        this.horarioFin = horarioFin;
-        this.precioMedioPersona = precioMedioPersona;
+        public Restaurante(String nombre, LocalDateTime horarioInicio, LocalDateTime horarioFin, double precioMedioPersona) throws ParametrosNoValidos {
+            this.nombre = nombre;
+            if (nombre == null) throw new ParametrosNoValidos("El nombre no puede ser nulo");
+            this.horarioInicio = horarioInicio;
+            if (horarioInicio == null) throw new ParametrosNoValidos("El nombre no puede ser nulo");
+            this.horarioFin = horarioFin;
+            if (horarioFin == null) throw new ParametrosNoValidos("El nombre no puede ser nulo");
+            if (horarioInicio.isAfter(horarioFin))
+                throw new ParametrosNoValidos("La hora de fin no puede ser anterior a la hora de inicio");
+            this.precioMedioPersona = precioMedioPersona;
+            if (precioMedioPersona <= 0)
+                throw new ParametrosNoValidos("El precio medio por persona no puede ser menor o igual a 0");
+        }
+
+        @Override
+        public String toString() {
+            return "Restaurante{" +
+                    "nombre='" + nombre + '\'' +
+                    ", horarioInicio=" + horarioInicio +
+                    ", horarioFin=" + horarioFin +
+                    ", precioMedioPersona=" + precioMedioPersona +
+                    '}';
+        }
     }
-
-    @Override
-    public String toString() {
-        return "Restaurante{" +
-                "nombre='" + nombre + '\'' +
-                ", horarioInicio=" + horarioInicio +
-                ", horarioFin=" + horarioFin +
-                ", precioMedioPersona=" + precioMedioPersona +
-                '}';
-    }
-}
