f74b9e5084a9f45adc02e02f91314416f71b6f19
AroaLMar <104788399+AroaLMar@users.noreply.github.com>
2022-06-10 01:07:09 +0200

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,36 +1,40 @@
 package com.company;
 
-
-
-import java.util.Date;
-
-
-public class Ticket {
-
-    private Date fecha;
-    private float precio;
-    private Cliente clientes;
+import java.time.LocalDate;
 
 
 
-    public Ticket(Date fecha, float precio, Cliente clientes) {
-        this.fecha= fecha;
-        this.precio = precio;
-        this.clientes = clientes;
+    public class Ticket {
+
+        private LocalDate fecha;
+        private float precio;
+        private Cliente clientes;
+
+
+
+        public Ticket(LocalDate fecha, float precio, Cliente clientes) throws ParametrosNoValidos {
+            this.fecha = fecha;
+            if(fecha==null) throw new ParametrosNoValidos("La fecha no puede ser nula");
+            this.precio = precio;
+            if(precio<=0) throw new ParametrosNoValidos("El precio no puede ser menor o igual a 0");
+            this.clientes = clientes;
+            if(clientes==null) throw new ParametrosNoValidos("El cliente no puede ser nulo");
+
+        }
+
+
+
+
+        @Override
+        public String toString() {
+            return "Ticket{" +
+                    "Fecha:" + fecha +
+                    ", Precio:" + precio +
+                    clientes
+                    +"}";
+
+        }
+
+
     }
-
-
-
-    @Override
-    public String toString() {
-        return "Ticket{" +
-                "Fecha:" + fecha +
-                ", Precio:" + precio +
-                clientes
-                +"}";
-
-    }
-
-
-}
 
